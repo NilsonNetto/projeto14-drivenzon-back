f35289b3b97bb54e8792326830afa1c4a826801c
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-09-13 19:28:15 -0300

    feat: Initial settings

---
+++ db/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+export default async function mongo() {
+
+  let dataBase;
+
+  try {
+    dataBase = await mongoClient.db('MyWallet-API');
+    return dataBase;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import mongo from "./db/db.js";
 
 const app = express();
 const port = 5000;
 
+
 app.use(express.json());
 app.use(cors());
+
+const db = await mongo();
 
 app.listen(port, () => {
   console.log(`Listen on port ${5000}`);
