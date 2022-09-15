57435873ce2363fd022e19ff92cb09c6ae0b52f4
NilsonNetto <eng.nilsonnetto@gmail.com>
2022-09-15 15:16:54 -0300

    chore: adjust code for deploy on heroku

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ import express from "express";
 import mongo from "./db/db.js";
 
 const app = express();
-const port = 5000;
-
 
 app.use(express.json());
 app.use(cors());
 
 const db = await mongo();
 
-app.listen(port, () => {
-  console.log(`Listen on port ${5000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listen on port ${process.env.PORT}`);
 });
